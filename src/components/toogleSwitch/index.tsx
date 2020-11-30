a4fb6318a5422411a31902babb206309fcf619f9
unknown <21.lucasmessias@gmail.com>
2020-11-30 12:04:07 -0300

    Finished the front design for create new orphanage.

---
+++ src/components/toogleSwitch/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './styles.css';
+
+interface iToogleSwitch {
+  setState: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const ToogleSwitch: React.FC<iToogleSwitch> = ({setState}) => {
+
+  const handlerFunction = (event: React.MouseEvent) => {
+    event.currentTarget.classList.toggle('active');
+    setState(event.currentTarget.classList[1] === 'active');
+  }
+
+  return <div 
+    className='toogle-switch'
+    onClick={handlerFunction}
+  />;
+}
+
+export default ToogleSwitch;
